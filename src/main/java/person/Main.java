3bf34d217654fd12a71165aed799fc97df1c38e5
Julien Smeets <j.smeets@quintor.nl>
2016-09-20 19:36:07 +0200

    Add exercises

--- src/main/java/person/Main.java
+++ src/main/java/person/Main.java
@@ -2,6 +2,7 @@ package person;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
@@ -10,10 +11,18 @@ import java.util.function.Predicate;
  * This application is supposed to enable an administrator to filter his personnel
  * based on various criteria, for example their age or gender.
  */
-
-// We eliminated both PersonTester and MasculinityTester
-
 public class Main {
+    // This is an adapted version of printPersons(). Instead of prescribing printing
+    // behavior, you can now provide a Consumer lambda that determines what is done with
+    // a Person object.
+    public static void filterAndProcessPersons(List<Person> persons,
+                                               Predicate<Person> tester,
+                                               Consumer<Person> consumer){
+        for (Person p : persons){
+            // TODO: Exercise 3.1. Apply the consumer if the Person p is tested positively
+        }
+    }
+
     public static void main (String[] args) {
         List<Person> persons = Arrays.asList(
                 new Person("Julien", 29, 189, Gender.MALE),
@@ -22,15 +31,12 @@ public class Main {
                 new Person("Ilse", 13, 151, Gender.FEMALE),
                 new Person("Jesse", 65, 190, Gender.OTHER));
 
-        // Instead of implementing PersonTester, we implemented Predicate<Person>,
-        // a generic interface that only declares method "boolean test()". These
-        // generic interfaces can be used with any Object type.
-        printPersons(persons, p -> p.gender == Gender.MALE);
-    }
-
-    // Prints persons when they match the criteria of the Predicate
-    public static void printPersons(List<Person> persons, Predicate<Person> tester){
-        for (Person p : persons) if (tester.test(p)) System.out.println(p);
+        // The processPersons method needs a Predicate and a Consumer.
+        // TODO: Exercise 3.2. Define lambda's so that the length of
+        // TODO: the name of everybody taller than 170 is printed.
+        processPersons(persons,
+                /**Predicate implementation here*/,
+                /**Consumer implementation here*/);
     }
 }
 
