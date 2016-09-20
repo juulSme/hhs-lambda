573f4eb1d51d1114916802acc79cd9fada286aac
Julien Smeets <j.smeets@quintor.nl>
2016-09-20 11:40:54 +0200

    Add answer

--- src/main/java/person/Main.java
+++ src/main/java/person/Main.java
@@ -24,6 +24,7 @@ public class Main {
     public static void printPersonsOlderThan(List<Person> persons, int age){
         // TODO: Exercise 1.1.
         // Implement this method to print Persons older than a certain age
+        for (Person p : persons) if (age > p.age) System.out.println(p);
     }
 
     // TODO: Exercise 1.2.
@@ -32,7 +33,12 @@ public class Main {
     // flexibility of this approach. You may write down your answer in the comment down below.
 
     /**
-     * MY ANSWER
+     * The downsides are that you need a new method for every filter and all of them will be
+     * tightly coupled to the Person class. For example, consider the changes needed if you
+     * implemented methods to print persons below a certain age, or within a range of ages, and
+     * then switched to bytes to record a Persons age. Additionally, it will take an
+     * incredible amount of code to provide the administrator with the ability to filter
+     * on different criteria.
      */
 }
 
