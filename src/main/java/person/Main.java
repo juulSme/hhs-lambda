2ce0da0094d9513a67bd6a07df811e734e0a7c89
Julien Smeets <j.smeets@quintor.nl>
2016-09-20 10:37:34 +0200

    Added exercises

--- src/main/java/person/Main.java
+++ src/main/java/person/Main.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 /**
  * Created by Julien Smeets (jsmeets@quintor.nl) on 8-9-16.
+ *
+ * This application is supposed to enable an administrator to filter his personnel
+ * based on various criteria, for example their age or gender.
  */
 public class Main {
     public static void main (String[] args) {
@@ -14,7 +17,23 @@ public class Main {
                 new Person("Abdul", 21, 180, Gender.MALE),
                 new Person("Ilse", 13, 151, Gender.FEMALE),
                 new Person("Jesse", 65, 190, Gender.OTHER));
+
+        printPersonsOlderThan(persons, 30);
     }
+
+    public static void printPersonsOlderThan(List<Person> persons, int age){
+        // TODO: Exercise 1.1.
+        // Implement this method to print Persons older than a certain age
+    }
+
+    // TODO: Exercise 1.2.
+    // The method you implemented in exercise 1 is a method that filters the list of persons.
+    // Come up with some drawbacks to this approach. Think about the brittleness and
+    // flexibility of this approach. You may write down your answer in the comment down below.
+
+    /**
+     * MY ANSWER
+     */
 }
 
 class Person {
