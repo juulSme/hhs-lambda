e356e3d20b0fca889119a86cb508bc3eb7ce9524
Julien Smeets <j.smeets@quintor.nl>
2016-09-20 10:01:26 +0200

    Initial commit

---
+++ src/main/java/person/Main.java
@@ -0,0 +1,39 @@
+package person;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Julien Smeets (jsmeets@quintor.nl) on 8-9-16.
+ */
+public class Main {
+    public static void main (String[] args) {
+        List<Person> persons = Arrays.asList(
+                new Person("Julien", 29, 189, Gender.MALE),
+                new Person("Peter", 35, 172, Gender.PRIVATE),
+                new Person("Abdul", 21, 180, Gender.MALE),
+                new Person("Ilse", 13, 151, Gender.FEMALE),
+                new Person("Jesse", 65, 190, Gender.OTHER));
+    }
+}
+
+class Person {
+    public String name;
+    public int age;
+    public int length;
+    public Gender gender;
+
+
+    public Person(String name, int age, int length, Gender gender){
+        this.name = name; this.age = age; this.length = length; this.gender = gender;
+    }
+
+    @Override
+    public String toString(){
+        return name + " is " + age + " years old, " + length + " cm long and has gender \"" + gender.toString() + "\".";
+    }
+}
+
+enum Gender {
+    MALE, FEMALE, OTHER, PRIVATE
+}
